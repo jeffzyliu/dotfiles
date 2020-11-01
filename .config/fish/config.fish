4190bf96fce1876ab6f35de666ad3510d8a0f973
Jeff Liu <jeffzyliu@gmail.com>
2020-11-01 18:23:11 -0500

    perl support

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,14 @@ end
 
 set -gx FZF_DEFAULT_COMMAND  'rg --files --no-ignore-vcs --hidden -g "!.git"'
 
+set -x PATH /Users/JeffLiu/perl5/bin $PATH ^/dev/null;
+set -q PERL5LIB; and set -x PERL5LIB /Users/JeffLiu/perl5/lib/perl5:$PERL5LIB;
+set -q PERL5LIB; or set -x PERL5LIB /Users/JeffLiu/perl5/lib/perl5;
+set -q PERL_LOCAL_LIB_ROOT; and set -x PERL_LOCAL_LIB_ROOT /Users/JeffLiu/perl5:$PERL_LOCAL_LIB_ROOT;
+set -q PERL_LOCAL_LIB_ROOT; or set -x PERL_LOCAL_LIB_ROOT /Users/JeffLiu/perl5:;
+set -x PERL_MB_OPT --install_base\ \"/Users/JeffLiu/perl5\";
+set -x PERL_MM_OPT INSTALL_BASE=/Users/JeffLiu/perl5;
+
 if status --is-interactive
     starship init fish | source
     fnm env --multi | source
